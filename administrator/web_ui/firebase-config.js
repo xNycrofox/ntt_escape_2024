a36f532467ca897e6b65c0116f2ef654e83608a6
Elias Müller <184137001+xNycrofox@users.noreply.github.com>
2024-10-23 15:57:32 +0000

    Remove API Key

--- administrator/web_ui/firebase-config.js
+++ administrator/web_ui/firebase-config.js
@@ -2,12 +2,7 @@
 
 // Firebase-Konfiguration
 const firebaseConfig = {
-    apiKey: "AIzaSyBhTCwVOAnNpLlQ9OkaZwKuEVzCeL5EbHE",
-    authDomain: "ntt-escape-2024.firebaseapp.com",
-    projectId: "ntt-escape-2024",
-    storageBucket: "ntt-escape-2024.appspot.com",
-    messagingSenderId: "866794486830",
-    appId: "1:866794486830:web:c0867fcd4e8d544851b3bf"
+    *censored*
   };
 
 // Firebase initialisieren
